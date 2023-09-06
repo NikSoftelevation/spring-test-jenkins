95b72594f7f226bab4da4f1b1533032907f9809e
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-09-06 17:20:00 +0530

    Created User Entity

---
+++ src/main/java/com/springtestjenkins/entity/User.java
@@ -0,0 +1,26 @@
+package com.springtestjenkins.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String password;
+}
